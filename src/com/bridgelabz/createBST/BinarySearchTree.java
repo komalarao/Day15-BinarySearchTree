affc182e43e2a6a00128db3e40d829dc64b7d196
komalarao <manoharvakamullu1999@gmail.com>
2023-01-29 23:18:05 -0800

    refactor: program for check BST IS FULL OR NOT using size method

--- src/com/bridgelabz/createBST/BinarySearchTree.java
+++ src/com/bridgelabz/createBST/BinarySearchTree.java
@@ -72,8 +72,36 @@ public class BinarySearchTree {
 
 		}
 
-		}
-
+		} // Function to return the size of binary tree
+	    int size() { return size(root); }
+	    
+	    /* computes number of nodes in tree */
+	    int size(INode node)
+	    {
+	        if (node == null) 
+	        	return 0;
+	        
+	        else 
+	            return (size(node.left) + 1 + size(node.right)); 
+	    } boolean isFullTree(INode node)
+	    {
+	        // if empty tree
+	        if(node == null)
+	        return true;
+	          
+	        // if leaf node
+	        if(node.left == null && node.right == null )
+	            return true;
+	          
+	        // if both left and right subtrees are not null
+	        // they are full
+	        if((node.left!=null) && (node.right!=null))
+	            return (isFullTree(node.left) && isFullTree(node.right));
+	          
+	        // if none work
+	        return false;
+	    }
+	 
 
 		public static void main(String[] args) {
 
@@ -82,9 +110,23 @@ public class BinarySearchTree {
 		tree.insert(56);
 		tree.insert(30);
 		tree.insert(70);
-		
-		tree.inorder();
-
+		tree.insert(60);
+		tree.insert(95);
+		tree.insert(65);
+		tree.insert(63);
+		tree.insert(67);
+		tree.insert(22);
+		tree.insert(40);
+		tree.insert(11);
+		tree.insert(3);
+		tree.insert(16);
+		tree.inorder();		
+        System.out.println("size of binary tree is "+tree.size());
+        
+        if(tree.isFullTree(tree.root))
+            System.out.print("The binary tree is full");
+        else
+            System.out.print("The binary tree is not full");
 		}
 
 }
